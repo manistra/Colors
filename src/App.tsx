74b164eee499520d3b52cfdb64500b9c2db42ca2
manistra <lor3nzo95@gmail.com>
2021-06-21 14:27:56 +0200

    tidying up css classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,40 @@
 function App() {
   return (
-    <div className='App antialiased'>
-      <div className='h-screen bg-blue-100'>
-        <div className='w-11/12 h-full mx-auto md:w-10/12 lg:w-8/12 xl:w-7/12'>
-          <div className='flex flex-col items-center justify-between h-1/2 bg-blue-200'>
-            <h1 className='lg:my-6 my-2 text-5xl lg:text-8xl md:text-8xl sm:text-6xl'>
-              Title
-            </h1>
+    <div className='antialiased App'>
+      <div className='main-container'>
+        <div className='top-container'>
+          <h1 className='my-2 lg:my-6 text-title'>Colors</h1>
 
-            <div className=' my-4 text-3xl tracking-wide uppercase lg:text-6xl md:text-4xl sm:text-3xl p-1 px-4 border-b-2 border-gray-900'>
-              #FFFFFF
-            </div>
+          <p className='p-1 px-4 my-4 border-b-2 border-gray-900 text-hex'>
+            #FFFFFF
+          </p>
 
-            <button className='my-1  h-14 px-4 w-9/12 lg:w-1/2 text-xl font-bold bg-white border-2 border-gray-900 rounded shadow-md lg:h-20 lg:text-3xl '>
-              Generate Color
-            </button>
-            <input
-              placeholder='Enter color hex'
-              type='text'
-              className='h-10 p-2 w-11/12 text-md lg:text-xl font-semibold uppercase border-2 border-gray-900  shadow-md rounded lg:h-12'
-            />
-          </div>
-          <div className='flex justify-center items-center h-1/2 bg-blue-300'>
-            <div className='w-full h-5/6 bg-blue-500'>
-              <ul className='overflow-y-scroll max-h-full bg-white border-2 uppercase border-gray-900 rounded-md shadow-xl'>
-                <li className='items-center flex m-1 p-2 cursor-pointer  bg-blue-300 rounded'>
-                  #FFFFFF
-                </li>{' '}
-                <li className='items-center flex m-1 p-2 cursor-pointer  bg-blue-300 rounded'>
-                  #FFFFFF
-                </li>{' '}
-                <li className='items-center flex m-1 p-2 cursor-pointer  bg-blue-300 rounded'>
-                  #FFFFFF
-                </li>{' '}
-                <li className='items-center flex m-1 p-2 cursor-pointer  bg-blue-300 rounded'>
-                  #FFFFFF
-                </li>{' '}
-                <li className='items-center flex m-1 p-2 cursor-pointer  bg-blue-300 rounded'>
-                  #FFFFFF
-                </li>{' '}
-                <li className='items-center flex m-1 p-2 cursor-pointer  bg-blue-300 rounded'>
-                  #FFFFFF
-                </li>{' '}
-                <li className='items-center flex m-1 p-2 cursor-pointer  bg-blue-300 rounded'>
-                  #FFFFFF
-                </li>{' '}
-                <li className='items-center flex m-1 p-2 cursor-pointer  bg-blue-300 rounded'>
-                  #FFFFFF
-                </li>{' '}
-                <li className='items-center flex m-1 p-2 cursor-pointer  bg-blue-300 rounded'>
-                  #FFFFFF
-                </li>{' '}
-                <li className='items-center flex m-1 p-2 cursor-pointer  bg-blue-300 rounded'>
-                  #FFFFFF
-                </li>{' '}
-                <li className='items-center flex m-1 p-2 cursor-pointer  bg-blue-300 rounded'>
-                  #FFFFFF
-                </li>
-              </ul>
-            </div>
+          <button className='w-9/12 px-4 my-1 btn lg:w-1/2'>
+            Generate Color
+          </button>
+          <input
+            placeholder='Enter color hex'
+            type='text'
+            className='w-11/12 input'
+          />
+        </div>
+        <div className='bottom-container'>
+          <div className='w-full h-5/6 '>
+            <ul className='list-container'>
+              <li className='p-2 m-1 list-item'>#FFFFFF</li>
+              <li className='p-2 m-1 list-item'>#FFFFFF</li>
+              <li className='p-2 m-1 list-item'>#FFFFFF</li>
+              <li className='p-2 m-1 list-item'>#FFFFFF</li>
+              <li className='p-2 m-1 list-item'>#FFFFFF</li>
+              <li className='p-2 m-1 list-item'>#FFFFFF</li>
+              <li className='p-2 m-1 list-item'>#FFFFFF</li>
+              <li className='p-2 m-1 list-item'>#FFFFFF</li>
+              <li className='p-2 m-1 list-item'>#FFFFFF</li>
+              <li className='p-2 m-1 list-item'>#FFFFFF</li>
+              <li className='p-2 m-1 list-item'>#FFFFFF</li>
+              <li className='p-2 m-1 list-item'>#FFFFFF</li>
+              <li className='p-2 m-1 list-item'>#FFFFFF</li>
+            </ul>
           </div>
         </div>
       </div>
